5150aff2c5d0bec47a255896de3f5851a5eda46f
pedro bufulin <pedro.almeida@ufu.br>
2022-01-28 17:18:02 -0300

    next round and set result emthods

---
+++ src/Tournament/nextRound.ts
@@ -0,0 +1,9 @@
+import { Options } from '../types/Options';
+import { Player } from '../types/Player';
+import { Tournament } from '../types/Tournament';
+
+export default function nextRound(tourney: Tournament): Tournament {
+  // pairs players accordingly
+
+  return tourney;
+}
---
+++ src/Tournament/setResult.ts
@@ -0,0 +1,13 @@
+import { Match } from '../types/Match';
+import { Options } from '../types/Options';
+import { Player } from '../types/Player';
+import { Tournament } from '../types/Tournament';
+
+export default function nextRound(
+  tourney: Tournament,
+  match: Match
+): Tournament {
+  // sets a result of a match
+
+  return tourney;
+}
