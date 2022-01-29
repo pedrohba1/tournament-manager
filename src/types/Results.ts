bea0dbbc0866b84260e9153ec000fd3378e0fa86
pedro bufulin <pedro.almeida@ufu.br>
2022-01-29 19:48:16 -0300

    results are number

--- src/types/Results.ts
+++ src/types/Results.ts
@@ -1,9 +1,7 @@
-export type matchResult = 0 | 1 | 2;
-
 export type Result = {
-  p1: matchResult;
-  p2: matchResult;
-  d: matchResult; //TODO: verificar se tem como ter 3 empates.
+  p1: number;
+  p2: number;
+  d: number; //TODO: verificar se tem como ter 3 empates.
 };
 
 export type Results = Result[];
