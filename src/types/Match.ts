699dda1bb7f73dc1f1067b680e0bb3cf6eb1c302
pedro bufulin <pedro.almeida@ufu.br>
2022-01-29 17:15:54 -0300

    remove unnecessary types in match

--- src/types/Match.ts
+++ src/types/Match.ts
@@ -3,15 +3,11 @@ import { Result } from './Results';
 
 export type Match = {
   matchNumber: number;
-  bestOf: number;
   round: number;
   playerOne: Player;
   playerTwo: Player;
-  active: boolean;
   result: Result;
-  playerOneWins: number;
-  playerTwoWins: number;
-  draws: number;
+  active: boolean;
   etc: unknown;
 };
 
