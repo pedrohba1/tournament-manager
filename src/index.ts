b8b70a7ae095bc1a8aa1b2a918d9169174aee886
pedro bufulin <pedro.almeida@ufu.br>
2022-01-29 17:16:15 -0300

    tests pairing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import createTourney from './Tournament/createTourney';
-import startTourney from './Tournament/startTourney';
-import setResult from './Tournament/setResult';
-import { Options } from './types/Options';
-import { Player } from './types/Player';
-import nextRound from './Tournament/nextRound';
-import calculateTiebreakers from './utils/calculateTiebreakers';
+import createTourney from '../src/Tournament/createTourney';
+import startTourney from '../src/Tournament/startTourney';
+import setResult from '../src/Tournament/setResult';
+import { Options } from '../src/types/Options';
+import { Player } from '../src/types/Player';
+import nextRound from '../src/Tournament/nextRound';
+import calculateTiebreakers from '../src/utils/calculateTiebreakers';
 
 const options = <Options>{
   seed: 1,
@@ -32,12 +32,18 @@ for (let i = 0; i < amount; i++) {
 }
 
 let tourney = createTourney(options, players);
+
 tourney = startTourney(tourney);
 
 tourney = setResult(tourney, 1, { d: 0, p1: 2, p2: 0 });
 tourney = setResult(tourney, 2, { d: 0, p1: 2, p2: 0 });
 
 tourney = nextRound(tourney);
+
+const newMatches = tourney.matches.filter(
+  (m) => m.round === tourney.currentRound
+);
+console.log(newMatches);
 
 for (const player of tourney.players) {
 }
