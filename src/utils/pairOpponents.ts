d4fe779213fbf73852d14bb592162b62e189e7fe
pedro bufulin <pedro.almeida@ufu.br>
2022-01-29 14:58:07 -0300

    sorts for from best to worst in pairings

--- src/utils/pairOpponents.ts
+++ src/utils/pairOpponents.ts
@@ -1,28 +1,40 @@
 import { Match, Matches } from '../types/Match';
 import { Tournament } from '../types/Tournament';
-import shuffle from './shuffle';
 
 export default function pairOpponents(tourney: Tournament): Tournament {
-  // // Shuffle players with seed
-  // const players = shuffle(tourney.players, seed);
-  // // create and assign matches
-  // const matches: Matches = [];
-  // for (let i = 0; i < players.length; i += 2) {
-  //   const match = <Match>{
-  //     playerOne: players[i],
-  //     // last will contain bye. Assign bye to player.
-  //     playerTwo: players[i + 1] ? players[i + 1] : { bye: true },
-  //     active: true,
-  //     draws: 0,
-  //     matchNumber: i,
-  //     playerOneWins: 0,
-  //     playerTwoWins: 0,
-  //     round: 1,
-  //     result: null,
-  //   };
-  //   matches.push(match);
-  // }
+  let pairingPlayers = tourney.players;
 
-  // tourney.matches = matches;
+  // first, removes from the array the players that have been dropped:
+  pairingPlayers = pairingPlayers.filter((p) => p.active === true);
+
+  // then orders players in descending order (first is best last is worst)
+  pairingPlayers.sort((a, b) => {
+    if (a.tiebreakers.matchPoints === b.tiebreakers.matchPoints) {
+      // if match points are the same, calculate based on opmwp's
+      if (a.tiebreakers.omwp !== b.tiebreakers.omwp) {
+        return a.tiebreakers.omwp < b.tiebreakers.omwp ? 1 : -1;
+      }
+
+      // if both match points and omwp's are the same, calculate based on gwp's
+      if (a.tiebreakers.gwp !== b.tiebreakers.gwp) {
+        return a.tiebreakers.gwp < b.tiebreakers.gwp ? 1 : -1;
+      }
+
+      // if it ties on matchPpoints, omwp's and gwp's, try to order by ogwp's
+      if (a.tiebreakers.ogwp !== b.tiebreakers.ogwp) {
+        return a.tiebreakers.ogwp < b.tiebreakers.ogwp ? 1 : -1;
+      }
+
+      // if none of the above work, order them in any way
+      return 1;
+    }
+
+    return a.tiebreakers.matchPoints < b.tiebreakers.matchPoints ? 1 : -1;
+  });
+
+  console.log(pairingPlayers);
+
+  // from pairingPlayers, create new matches were the best are paired with the best and the worst with the worts
+
   return tourney;
 }
