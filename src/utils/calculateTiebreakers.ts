03599bf6f1593990c530768493c093e1ba063d46
pedro bufulin <pedro.almeida@ufu.br>
2022-01-28 23:16:01 -0300

    all important tiebreakers calculations

--- src/utils/calculateTiebreakers.ts
+++ src/utils/calculateTiebreakers.ts
@@ -5,59 +5,105 @@ export default function calculateTiebreakers(
   player: Player,
   tourney: Tournament
 ): Player {
-  // calculate omw, ogw, pgw percentage based on all match results
-  // filter matches that his player participated
   const { tiebreakers } = player;
   const { currentRound } = tourney;
   const { matchPoints, gamePoints } = tiebreakers;
-  // Match Points: Players earn 3 match points for each match win and 1 match point for each match ending in a draw. (No points are awarded for a match loss.)
-  // Game Points: Game points are similar to match points in that players earn 3 game points for each game they win and 1 point for each game that ends in a draw
   const OpponentsMatchWin = [];
   const OpponentsGameWin = [];
   const PlayerResults = [];
 
+  // filter matches that his player participated
   const playerMatches = tourney.matches.filter(
     (item) => item.playerOne.id === player.id || item.playerTwo.id === player.id
   );
 
-  const opponentsNoBye = tourney.matches
-    .filter(
-      (item) =>
-        (item.playerOne.id === player.id || item.playerTwo.id === player.id) &&
-        (!item.playerOne.bye || !item.playerTwo.bye)
-    )
-    .map((match) => {
-      const opponentSide =
-        match.playerOne.id === player.id ? 'playerTwo' : 'playerOne';
-      if (!match[opponentSide].bye) {
-        OpponentsMatchWin.push(match[opponentSide].tiebreakers.mwp);
-        OpponentsGameWin.push(match[opponentSide].tiebreakers.gwp);
-      }
-    });
-
-  // Match-win percentage: A players match-win percentage is that play accumuated match points divided by 3 times the number of rounds in which he or she competed, or 0.33
+  /**
+   * Match-win percentage:
+   * A players match-win percentage is that
+   * play accumulated match points divided by 3 times
+   * the number of rounds in which he or she competed, or 0.33
+   */
   const mwp =
-    matchPoints / (currentRound * 3) < 0.33
+    matchPoints / (playerMatches.length * 3) < 0.33
       ? 0.33
-      : matchPoints / (currentRound * 3);
-  // Game-win percentage: Similar to the match-win percentage, a players game-win percentage is the total number of game points he or she earned divided by 3 times the number of games played.
+      : matchPoints / (playerMatches.length * 3);
+
+  /**
+   * Opponent match-win percentage: A players opponents match-win percentage
+   * is the average match-win percentage of each opponent that
+   * player faced (ignoring those rounds for which the player received a bye)
+   */
+  let acc = 0;
+  for (const match of playerMatches) {
+    // get single oppopnet (desconsider byes)
+    const opponent =
+      match.playerOne.id !== player.id ? match.playerOne : match.playerTwo;
+    if (opponent.bye === true) continue;
+
+    // get opponent matches
+    const opponentMatches = tourney.matches.filter(
+      (m) => m.playerOne.id === player.id || m.playerTwo.id === player.id
+    );
+
+    // calculate opponent match win percentage and sum in the accumulator
+    const {
+      tiebreakers: { matchPoints },
+    } = tourney.players.find((p) => p.id === opponent.id);
+    acc +=
+      matchPoints / (opponentMatches.length * 3) < 0.33
+        ? 0.33
+        : matchPoints / (opponentMatches.length * 3);
+  }
+  const omwp = acc / playerMatches.length;
+
+  /**
+   * Game-win percentage:
+   * Similar to the match-win percentage,
+   * a players game-win percentage is the total
+   * number of game points he or she earned divided by 3 times the number of games played.
+   */
+  const { w, l, d } = player.tiebreakers.gamesSummary;
+  const allGames = w + l + d;
   const gwp =
-    gamePoints / (playerMatches.length * 3) < 0.33
-      ? 0.33
-      : gamePoints / (playerMatches.length * 3);
-  // OMW% = (Each Opponents Match Win Percentage added) / Number of opponens
-  // Opponent match-win percentage: A players opponents match-win percentage is the average match-win percentage of each opponent that player faced (ignoring those rounds for which the player received a bye)
-  const omw = OpponentsMatchWin.reduce((a, b) => a + b, 0);
-  // Opponent game-win percentages: Similar to opponents match-win percentage, a players opponents game-win percentage is simply the average game-win percentage of all of that players opponents. And, as with opponents match-win percentage, each opponent has a minimum game-win percentage of 0.33.
-  const ogw = OpponentsGameWin.reduce((a, b) => a + b, 0);
+    gamePoints / (allGames * 3) < 0.33 ? 0.33 : gamePoints / (allGames * 3);
+
+  /**
+   *  Opponents game-win percentages:
+   * Similar to opponents’ match-win percentage, a player’s opponents’ game-win percentage is sim-
+   * ply the average game-win percentage of all of that player’s opponents. And, as with opponents’
+   * match-win percentage, each opponent has a minimum game-win percentage of 0.33.
+   */
+  let acc2 = 0;
+  for (const match of playerMatches) {
+    // get single oppopnet (desconsider byes)
+    const opponent =
+      match.playerOne.id !== player.id ? match.playerOne : match.playerTwo;
+    if (opponent.bye === true) continue;
+
+    // get opponent matches
+    const opponentMatches = tourney.matches.filter(
+      (m) => m.playerOne.id === player.id || m.playerTwo.id === player.id
+    );
+
+    // calculate opponent game win percentage and sum in the accumulator
+    const {
+      tiebreakers: { gamesSummary, gamePoints },
+    } = tourney.players.find((p) => p.id === opponent.id);
+    const { w, l, d } = gamesSummary;
+    const allGames = w + l + d;
+
+    acc2 +=
+      gamePoints / (allGames * 3) < 0.33 ? 0.33 : gamePoints / (allGames * 3);
+  }
+  const ogwp = acc2 / playerMatches.length;
 
   player.tiebreakers = {
     ...tiebreakers,
-    mwp: mwp,
-    gwp: gwp,
-    omwp: omw,
-    ogwp: ogw,
-    pgw: 420,
+    mwp,
+    omwp,
+    gwp,
+    ogwp,
+    pgw: 0,
   };
 
   return player;
