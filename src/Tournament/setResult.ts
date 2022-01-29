6899c4799b2153b5adbbe4a8b3c3abc27a359119
pedro bufulin <pedro.almeida@ufu.br>
2022-01-29 18:29:36 -0300

    remove line

--- src/Tournament/setResult.ts
+++ src/Tournament/setResult.ts
@@ -14,7 +14,6 @@ export default function setResult(
     (m) => m.matchNumber === matchNumber
   );
   if (!tourney.matches[matchIndex]) throw Error('match does not exist');
-
   if (tourney.matches[matchIndex].round !== tourney.currentRound)
     throw Error("can't change result of a past match");
   tourney.matches[matchIndex].result = result;
