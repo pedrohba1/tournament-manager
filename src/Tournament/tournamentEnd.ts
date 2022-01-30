662d37f6539fbf0b1a70827ac4958814d00bb68b
pedro bufulin <pedro.almeida@ufu.br>
2022-01-30 14:50:56 -0300

    set player standings when tournament ends

--- src/Tournament/tournamentEnd.ts
+++ src/Tournament/tournamentEnd.ts
@@ -4,10 +4,19 @@ import { Player } from '../types/Player';
 import { Result } from '../types/Results';
 import { Tournament } from '../types/Tournament';
 import getStandings from '../utils/getStandings';
+import setPlayersPoints from '../utils/setPlayerPoints';
+import calculateTiebreakers from '../utils/calculateTiebreakers';
 
 export default function tournamentEnd(tourney: Tournament): Player[] {
   if (tourney.currentRound !== tourney.options.maxRounds)
     throw Error('not in final round');
+
+  tourney = setPlayersPoints(tourney);
+  // calculate tiebreakers for each player based on points set
+  for (const player of tourney.players) {
+    const playerIndex = tourney.players.findIndex((p) => p.id === player.id);
+    tourney.players[playerIndex] = calculateTiebreakers(player, tourney);
+  }
 
   const standings = getStandings(tourney);
   return standings;
