c3dc504c4d1240891f5376ab0066c742ef4d0add
pedro bufulin <pedro.almeida@ufu.br>
2022-01-30 16:06:09 -0300

    pair seed based only if seed exists

--- src/utils/pairOpponentsFirstRound.ts
+++ src/utils/pairOpponentsFirstRound.ts
@@ -4,10 +4,10 @@ import shuffle from './shuffle';
 
 export default function pairOpponentsFirstRound(
   tourney: Tournament,
-  seed: number
+  seed?: number
 ): Tournament {
   // Shuffle players with seed
-  const players = shuffle(tourney.players, seed);
+  const players = seed ? shuffle(tourney.players, seed) : tourney.players;
   // create and assign matches
   const matches: Matches = [];
   for (let i = 0; i < players.length; i += 2) {
