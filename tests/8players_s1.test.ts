87cfa9337d5db0b34fa0b7d31aee6ade8f1aab3c
pedro bufulin <pedro.almeida@ufu.br>
2022-01-30 17:16:47 -0300

    validates partial standings

--- tests/8players_s1.test.ts
+++ tests/8players_s1.test.ts
@@ -37,20 +37,92 @@ describe('Filter function', () => {
 
     const players = <Player[]>[];
     const amount = 8;
-    for (let i = 0; i < amount; i++) {
-      const player = <Player>{
-        id: `${i}`,
-        nickname: `user_${i}`,
-        name: `name_${i}`,
-      };
-      players.push(player);
-    }
+
+    players.push(<Player>{
+      id: '5',
+      nickname: 'user_5',
+      name: 'user_5',
+    });
+
+    players.push(<Player>{
+      id: '4',
+      nickname: 'user_4',
+      name: 'user_4',
+    });
+
+    players.push(<Player>{
+      id: '3',
+      nickname: 'user_3',
+      name: 'user_3',
+    });
+
+    players.push(<Player>{
+      id: '2',
+      nickname: 'user_2',
+      name: 'user_2',
+    });
+
+    players.push(<Player>{
+      id: '1',
+      nickname: 'user_1',
+      name: 'user_1',
+    });
+
+    players.push(<Player>{
+      id: '0',
+      nickname: 'user_0',
+      name: 'user_0',
+    });
+
+    players.push(<Player>{
+      id: '6',
+      nickname: 'user_6',
+      name: 'user_6',
+    });
+
+    players.push(<Player>{
+      id: '7',
+      nickname: 'user_7',
+      name: 'user_7',
+    });
 
     tourney = createTourney(options, players);
     expect(tourney.players.length).toBe(8);
   });
 
-  it('should start tourney and assign matches', () => {
+  it('hould start tourney and assign matches', () => {
     tourney = startTourney(tourney);
+  });
+
+  it('should assign results', () => {
+    //user5 2x0 user4
+    tourney = setResult(tourney, 1, { p1: 2, p2: 0, d: 0 });
+    //user3 1x1 1draw user2
+    tourney = setResult(tourney, 2, { p1: 1, p2: 1, d: 1 });
+    //user1 2x1 user0
+    tourney = setResult(tourney, 3, { p1: 2, p2: 1, d: 0 });
+    //user6 0x2 user7
+    tourney = setResult(tourney, 4, { p1: 0, p2: 2, d: 0 });
+    console.log(tourney.matches);
+  });
+
+  it('should start next round', () => {
+    tourney = nextRound(tourney);
+  });
+
+  it('should get standings for round 1', () => {
+    const standings = getStandings(tourney.players);
+    console.log('round 1 standings:');
+    for (const standing of standings) {
+      console.table({ ...standing.tiebreakers, nickname: standing.nickname });
+    }
+    expect(standings[0].nickname).toBe('user_7');
+    expect(standings[1].nickname).toBe('user_5');
+    expect(standings[2].nickname).toBe('user_1');
+    expect(standings[3].nickname).toBe('user_2');
+    expect(standings[4].nickname).toBe('user_3');
+    expect(standings[5].nickname).toBe('user_0');
+    expect(standings[6].nickname).toBe('user_6');
+    expect(standings[7].nickname).toBe('user_4');
   });
 });
