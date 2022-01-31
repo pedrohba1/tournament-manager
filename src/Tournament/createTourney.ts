c5090592d7486f0d0af92eece31c1fc59379df23
pedro bufulin <pedro.almeida@ufu.br>
2022-01-31 16:33:38 -0300

    start etc

--- src/Tournament/createTourney.ts
+++ src/Tournament/createTourney.ts
@@ -11,6 +11,7 @@ export default function createTourney(
   tourney.options = options;
   tourney.lastMatchNumber = 1;
   tourney.currentRound = 1;
+  tourney.etc = {};
   tourney.players = players.map((p) => ({
     ...p,
     active: true,
