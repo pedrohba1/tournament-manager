2bccf71126dacad1b307a4aa854e7aab6faf18b0
pedro bufulin <pedro.almeida@ufu.br>
2022-01-31 11:34:41 -0300

    gwp minimum is 0.33

--- src/utils/calculateTiebreakers.ts
+++ src/utils/calculateTiebreakers.ts
@@ -64,7 +64,8 @@ export default function calculateTiebreakers(
    */
   const { w, l, d } = player.tiebreakers.gamesSummary;
   const allGames = w + l + d;
-  const gwp = gamePoints / (allGames * 3);
+  const gwp =
+    gamePoints / (allGames * 3) < 0.33 ? 0.33 : gamePoints / (allGames * 3);
 
   /**
    *  Opponent's game-win percentages:
