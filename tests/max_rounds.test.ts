f3651d9f03b67d7ea4e2da703d0a9752702c0dd1
pedro bufulin <pedro.almeida@ufu.br>
2022-01-31 11:59:59 -0300

    test max round

---
+++ tests/max_rounds.test.ts
@@ -0,0 +1,53 @@
+import createTourney from '../src/Tournament/createTourney';
+import startTourney from '../src/Tournament/startTourney';
+import setResult from '../src/Tournament/setResult';
+import { Options } from '../src/types/Options';
+import { Player } from '../src/types/Player';
+import nextRound from '../src/Tournament/nextRound';
+import tournamentEnd from '../src/Tournament/tournamentEnd';
+import { Tournament } from '../src';
+import getStandings from '../src/utils/getStandings';
+import console from 'console';
+const jestConsole = console;
+
+describe('Filter function', () => {
+  beforeEach(() => {
+    global.console = require('console');
+  });
+
+  afterEach(() => {
+    global.console = jestConsole;
+  });
+
+  let tourney: Tournament;
+
+  it('should configurate a tournament', () => {
+    const options = <Options>{
+      seed: 10,
+      format: 'swiss',
+      gameType: 'magic',
+      playoffs: false,
+      cutLimit: 8,
+      maxRounds: null,
+      bestOf: 3,
+      winValue: 3,
+      maxRound: null,
+      drawValue: 1,
+      lossValue: 0,
+    };
+
+    const players = <Player[]>[];
+    const amount = 4;
+    for (let i = 0; i < amount; i++) {
+      const player = <Player>{
+        id: `${i}`,
+        nickname: `user_${i}`,
+        name: `name_${i}`,
+      };
+      players.push(player);
+    }
+
+    tourney = createTourney(options, players);
+    expect(tourney.options.maxRounds).toBe(2);
+  });
+});
