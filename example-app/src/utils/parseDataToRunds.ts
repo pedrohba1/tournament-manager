116a7625235d8ea16d865fe974c9589501be8da3
pedro bufulin <pedro.almeida@ufu.br>
2022-12-06 19:12:00 -0300

    version bump

---
+++ example-app/src/utils/parseDataToRunds.ts
@@ -0,0 +1,36 @@
+
+import { Match } from "@testing-library/react";
+import { IRoundProps } from "react-brackets";
+
+export const parseDataToRounds = (matches?: Match[]): IRoundProps[] => {
+  if (!matches) return [];
+  const rounds: IRoundProps[] = [];
+
+  matches.forEach(match => {
+    if (!rounds[match.round]) {
+      rounds[match.round] = {
+        title: `Round ${match.round}`,
+        seeds: [],
+      };
+    }
+
+    const seed = {
+      id: match.playerOne.id + match.playerTwo.id,
+      teams: [
+        { name: match.playerOne.nickname },
+        { name: match.playerTwo.nickname },
+      ],
+    };
+
+    rounds[match.round].seeds.push(seed);
+  });
+
+  const filteredRounds = rounds.filter(round => {
+    if (round.seeds.length <= 8) {
+      return round;
+    }
+    return undefined;
+  });
+
+  return filteredRounds;
+};
