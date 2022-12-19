06d3717c132a96d86cf1ff8ab79e0971114d3bfa
Leandro <leandrofontellas@gmail.com>
2022-12-19 07:19:12 -0300

    feat: updating css of example-app

--- example-app/src/utils/parseDataToRunds.ts
+++ example-app/src/utils/parseDataToRunds.ts
@@ -25,6 +25,34 @@ export const parseDataToRounds = (matches?: Match[]): IRoundProps[] => {
     rounds[match.round].seeds.push(seed);
   });
 
+  // NOTE manually pushing rounds to represent a full tourney, remove it if you need
+  rounds.push({
+    title: "Round 2",
+    seeds: [
+      {
+        id: 1,
+        date: new Date().toDateString(),
+        teams: [{ name: 'user_2' }, { name: 'user_0' }],
+      },
+      {
+        id: 2,
+        date: new Date().toDateString(),
+        teams: [{ name: 'user_1' }, { name: 'user_4' }],
+      },
+    ]
+  })
+
+  rounds.push({
+    title: "Round 3",
+    seeds: [
+      {
+        id: 1,
+        date: new Date().toDateString(),
+        teams: [{ name: 'user_2' }, { name: 'user_1' }],
+      }
+    ]
+  })
+
   const filteredRounds = rounds.filter(round => {
     if (round.seeds.length <= 8) {
       return round;
